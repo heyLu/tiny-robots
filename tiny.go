573f812cd55d99605958dd4612452346fd863a52
Lucas Stadler <luc@spreadshirt.net>
2017-07-05 15:20:42 +0200

    Add a simple !gif command

--- tiny.go
+++ tiny.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os/exec"
 	"strings"
 	"time"
@@ -13,13 +15,15 @@ import (
 )
 
 var config struct {
-	endpoint string
-	botEmail string
+	endpoint    string
+	botEmail    string
+	giphyAPIKey string
 }
 
 func init() {
 	flag.StringVar(&config.endpoint, "endpoint", "https://zulip.papill0n.org", "The URL of the Zulip instance")
 	flag.StringVar(&config.botEmail, "bot", "announcy-bot@zulip.papill0n.org", "The email address of the bot")
+	flag.StringVar(&config.giphyAPIKey, "giphy", "", "The API key for Giphy")
 }
 
 func main() {
@@ -57,12 +61,46 @@ func main() {
 					log.Println("sending message", err)
 					return
 				}
+			case strings.HasPrefix(ev.Content, "!gif"):
+				search := "elephant" // error elephant
+				fs := strings.Fields(ev.Content)
+				if len(fs) >= 2 {
+					search = fs[1]
+				}
+				gifInfo, err := getJSON("https://api.giphy.com/v1/gifs/random?api_key=" + config.giphyAPIKey + "&tag=" + search)
+				if err != nil {
+					log.Println("random gif:", err)
+					return
+				}
+				url := gifInfo.(map[string]interface{})["data"].(map[string]interface{})["image_url"].(string)
+				r := ev.Reply(fmt.Sprintf("here's some %s: %s", search, url))
+				err = client.Send(r)
+				if err != nil {
+					log.Println("sending message", err)
+				}
 			}
 		case zulip.Heartbeat:
 		default:
 			log.Println("unhandled message")
 		}
 	})
+}
+
+func getJSON(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	dec := json.NewDecoder(resp.Body)
+	var res interface{}
+	err = dec.Decode(&res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
 
 func onEachEvent(client *zulip.Client, handle func(zulip.Event)) {
